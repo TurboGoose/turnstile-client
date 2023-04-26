b6514210d503f758c3112ef582ad35c3ef56609d
TurboGoose <antonpuckich@gmail.com>
2023-04-26 11:39:01 +0300

    Delete BatchLoader from storage.py

--- cloud/storage.py
+++ cloud/storage.py
@@ -80,29 +80,3 @@ def get_info_by_id(id):
     cur.close()
     con.close()
     return info
-
-
-class BatchLoader:
-    def __init__(self, page_size):
-        self.page_size = page_size
-        self.cur_page = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.total_rows > self.page_size * self.cur_page:
-            raise StopIteration()
-        offset = self.cur_page * self.page_size
-        res = self.cur.execute(f"SELECT id, faceEncoding FROM faces LIMIT {self.page_size} OFFSET {offset};")
-        self.cur_page += 1
-        return res.fetchall()
-
-    def __enter__(self):
-        self.con = sqlite3.connect(DATABASE, detect_types=sqlite3.PARSE_DECLTYPES)
-        self.cur = self.con.cursor()
-        self.total_rows = self.cur.execute("SELECT count(*) FROM faces;").fetchone()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.cur.close()
-        self.con.close()
