8518abc532d9e42550d20f04e9c13a8787d6b234
TurboGoose <antonpuckich@gmail.com>
2023-05-04 18:06:05 +0300

    Delete face save functionality from edge

--- edge/core/client.py
+++ edge/core/client.py
@@ -24,26 +24,6 @@ def recognition_request(face_encoding):
         print(err)
 
 
-def save_request(name, surname, face_encoding):
-    url = url_template + "/save"
-
-    body = {
-        "name": name,
-        "surname": surname,
-        "encoding": face_encoding.tolist()
-    }
-    print("POST", url)
-
-    response = post(url, json=body)
-    print(f"Response {response.status_code}: {response.json()}")
-
-    if response.status_code != 200:
-        return None
-    try:
-        return response.json()
-    except JSONDecodeError as err:
-        print(err)
-
 def reload_request():
     url = url_template + "/reload"
     print("GET", url)
